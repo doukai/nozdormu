da45980bf4c7c98037d88d1c22404b4a093bd756
doukai <doukai.java@gmail.com>
2024-01-22 16:38:13 +0800

    Update TypesafeConfigProducer.java

--- nozdormu-config/src/main/java/io/nozdormu/config/produces/TypesafeConfigProducer.java
+++ nozdormu-config/src/main/java/io/nozdormu/config/produces/TypesafeConfigProducer.java
@@ -3,14 +3,23 @@ package io.nozdormu.config.produces;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigBuilder;
 import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 
 @ApplicationScoped
 public class TypesafeConfigProducer {
 
+    private static final ConfigProviderResolver configProviderResolver = ConfigProviderResolver.instance();
+
     @ApplicationScoped
     @Produces
     public Config config() {
-        return ConfigProviderResolver.instance().getConfig();
+        return configProviderResolver.getConfig();
+    }
+
+    @ApplicationScoped
+    @Produces
+    public ConfigBuilder configBuilder() {
+        return configProviderResolver.getBuilder();
     }
 }
