304cd821cc975415eb7ad6126090dc445a5ef3aa
doukai <doukai.java@gmail.com>
2024-07-10 22:54:56 +0800

    update

--- nozdormu-spi/src/main/java/io/nozdormu/spi/error/InjectionProcessErrorType.java
+++ nozdormu-spi/src/main/java/io/nozdormu/spi/error/InjectionProcessErrorType.java
@@ -2,23 +2,23 @@ package io.nozdormu.spi.error;
 
 public enum InjectionProcessErrorType {
 
-    ROOT_PACKAGE_NOT_EXIST(-101, "can't find root package"),
-    CANNOT_PARSER_SOURCE_CODE(-102, "can't parser source code: %s"),
-    CANNOT_GET_COMPILATION_UNIT(-103, "can't get compilation unit of %s"),
-    CLASS_NOT_EXIST(-104, "class not exist in: %s"),
-    PUBLIC_CLASS_NOT_EXIST(-104, "public class not exist in: %s"),
-    PUBLIC_ANNOTATION_NOT_EXIST(-105, "public annotation not exist in: %s"),
-    CONSTRUCTOR_NOT_EXIST(-106, "can't find constructor of %s"),
-    PROVIDER_TYPE_NOT_EXIST(-107, "can't find type argument of provider"),
-    INSTANCE_TYPE_NOT_EXIST(-108, "can't find type argument of instance"),
-    MODULE_PROVIDERS_METHOD_NOT_EXIST(-109, "can't find module class providers method of %s"),
-    COMPONENT_GET_METHOD_NOT_EXIST(-110, "can't find component class get method of %s"),
-    TYPE_ARGUMENT_NOT_EXIST(-111, "can't find type argument"),
+    ROOT_PACKAGE_NOT_EXIST(-60637, "can't find root package"),
+    CANNOT_PARSER_SOURCE_CODE(-60638, "can't parser source code: %s"),
+    CANNOT_GET_COMPILATION_UNIT(-60639, "can't get compilation unit of %s"),
+    CLASS_NOT_EXIST(-60640, "class not exist in: %s"),
+    PUBLIC_CLASS_NOT_EXIST(-60641, "public class not exist in: %s"),
+    PUBLIC_ANNOTATION_NOT_EXIST(-60642, "public annotation not exist in: %s"),
+    CONSTRUCTOR_NOT_EXIST(-60643, "can't find constructor of %s"),
+    PROVIDER_TYPE_NOT_EXIST(-60644, "can't find type argument of provider"),
+    INSTANCE_TYPE_NOT_EXIST(-60645, "can't find type argument of instance"),
+    MODULE_PROVIDERS_METHOD_NOT_EXIST(-60646, "can't find module class providers method of %s"),
+    COMPONENT_GET_METHOD_NOT_EXIST(-60647, "can't find component class get method of %s"),
+    TYPE_ARGUMENT_NOT_EXIST(-60649, "can't find type argument"),
 
-    CONFIG_PROPERTIES_PREFIX_NOT_EXIST(-121, "prefix not exist in @ConfigProperties in: %s"),
-    CONFIG_PROPERTY_NOT_EXIST(-122, "@ConfigProperty not exist on: %s"),
+    CONFIG_PROPERTIES_PREFIX_NOT_EXIST(-60700, "prefix not exist in @ConfigProperties in: %s"),
+    CONFIG_PROPERTY_NOT_EXIST(-60701, "@ConfigProperty not exist on: %s"),
 
-    UNKNOWN(-199, "unknown injection error");
+    UNKNOWN(-60999, "unknown injection error");
 
     private final int code;
     private final String description;
