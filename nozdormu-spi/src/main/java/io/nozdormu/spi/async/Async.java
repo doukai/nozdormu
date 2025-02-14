4a4b894c959bfeba40ea21c00878b1c878e25369
doukai <doukai.java@gmail.com>
2025-02-15 01:49:06 +0800

    Update Async.java

--- nozdormu-spi/src/main/java/io/nozdormu/spi/async/Async.java
+++ nozdormu-spi/src/main/java/io/nozdormu/spi/async/Async.java
@@ -3,7 +3,7 @@ package io.nozdormu.spi.async;
 import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Async {
     String defaultIfEmpty() default "";
